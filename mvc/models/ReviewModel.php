ab2492842b285dcc0cfcbb3c46f57d25aa09da8b
devcuong <cuong.dh8c@gmail.com>
2020-02-20 16:16:39 +0700

    ok

--- mvc/models/ReviewModel.php
+++ mvc/models/ReviewModel.php
@@ -2,7 +2,7 @@
 class ReviewModel extends DB{
     // Lấy review bằng id công ty
     public function LayReviewBangIdCongTy($iDCongTy){
-        $qr = "SELECT review.id AS review_id, review.reviewer AS review_reviewer, review.position AS review_position, review.rate AS review_rate, review.noidung AS review_noidung, review.congty AS review_congty, review.thoigian AS review_thoigian, reply.id AS reply_id, reply.idreview AS reply_idreview, reply.data AS reply_data FROM review LEFT JOIN reply on reply.idreview = review.id WHERE review.congty = $iDCongTy";
+        $qr = "SELECT review.id AS review_id, review.reviewer AS review_reviewer, review.position AS review_position, review.rate AS review_rate, review.noidung AS review_noidung, review.congty AS review_congty, review.thoigian AS review_thoigian, reply.id AS reply_id, reply.idreview AS reply_idreview, reply.data AS reply_data FROM review LEFT JOIN reply on reply.idreview = review.id WHERE review.congty = $iDCongTy ORDER BY review.id DESC";
         return mysqli_query($this->con, $qr);
     }
     
--- rvcongty.sql
+++ rvcongty.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 20, 2020 lúc 04:47 AM
+-- Thời gian đã tạo: Th2 20, 2020 lúc 10:15 AM
 -- Phiên bản máy phục vụ: 10.4.10-MariaDB
 -- Phiên bản PHP: 7.3.12
 
