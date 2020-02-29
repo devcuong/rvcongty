872d8cbac4b607958b6d556766095e507b3e0b08
devcuong <cuong.dh8c@gmail.com>
2020-02-29 08:42:25 +0700

    ok

--- mvc/views/admin-template.php
+++ mvc/views/admin-template.php
@@ -30,7 +30,7 @@
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script type="text/javascript">
-
+	
     </script>
 </head>
 <body>
