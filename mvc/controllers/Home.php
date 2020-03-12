383f384ce817c28b4fd9fbcf475f597bf1bb3437
devcuong <viettxt@gmail.com>
2020-03-12 16:34:37 +0700

    ok

--- mvc/controllers/Home.php
+++ mvc/controllers/Home.php
@@ -1,41 +1,49 @@
 <?php
-    class Home extends Controller{
-        function Index(){
+
+class Home extends Controller
+{
+
+    function Index($a = NULL)
+    {
+        if ($a == NULL) {
             $trangHienTai = 1;
             $congTyMoiTrang = 15;
-            $tabCongTy="latest";
-            if(isset($_GET["page"])){
+            $tabCongTy = "latest";
+            if (isset($_GET["page"])) {
                 $trangHienTai = $_GET["page"];
             }
-            if(isset($_GET["tab"])){
+            if (isset($_GET["tab"])) {
                 $tabCongTy = $_GET["tab"];
             }
-            $soCongTyBoQua = ($trangHienTai-1)*$congTyMoiTrang;
+            $soCongTyBoQua = ($trangHienTai - 1) * $congTyMoiTrang;
             // Model
             $congty = $this->model("CongTyModel");
             $review = $this->model("ReviewModel");
             // Tất cả công ty
             $tatCaCongTy = $congty->TatCaCongTy();
             $soCongTy = mysqli_num_rows($tatCaCongTy);
-            $soTrang = ceil($soCongTy/$congTyMoiTrang);
-            $congTyTrangHienTai="";
-            if($tabCongTy == "latest"){
-            $congTyTrangHienTai = $congty->LayCongTyPhanTrang($soCongTyBoQua, $congTyMoiTrang);
-            }else if($tabCongTy == "best"){
-                $congTyTrangHienTai=$congty->LayCongTyBestPhanTrang($soCongTyBoQua, $congTyMoiTrang);
-            }else if($tabCongTy == "worst"){
-                $congTyTrangHienTai=$congty->LayCongTyWorstPhanTrang($soCongTyBoQua, $congTyMoiTrang);
-            }
+            $soTrang = ceil($soCongTy / $congTyMoiTrang);
+            $congTyTrangHienTai = "";
+            if ($tabCongTy == "latest") {
+                $congTyTrangHienTai = $congty->LayCongTyPhanTrang($soCongTyBoQua, $congTyMoiTrang);
+            } else 
+                if ($tabCongTy == "best") {
+                    $congTyTrangHienTai = $congty->LayCongTyBestPhanTrang($soCongTyBoQua, $congTyMoiTrang);
+                } else 
+                    if ($tabCongTy == "worst") {
+                        $congTyTrangHienTai = $congty->LayCongTyWorstPhanTrang($soCongTyBoQua, $congTyMoiTrang);
+                    }
             
             // Title
             $title = "Review lương bổng, đãi ngộ, tuyển dụng, sếp của các công ty - CongTyTop";
             
             // Description
             $description = "Trang Review đầy đủ nhất về lương bổng, đãi ngộ, tuyển dụng, sếp của các công ty IT và liên quan IT - CongTyTop";
-     
+            
             $keyword = "review công ty, review cong ty, công ty review, cong ty review, review công việc, review cong viec, review mức lương, review muc luong, review sếp, review sep";
-            //View
-            $this->view("main-template", ["Page"=>"main-home",
+            // View
+            $this->view("main-template", [
+                "Page" => "main-home",
                 "15ReviewMoiNhat" => $review->Lay15ReviewMoiNhat(),
                 "SoTrang" => $soTrang,
                 "TrangHienTai" => $trangHienTai,
@@ -46,6 +54,10 @@
                 "Keyword" => $keyword
             ]);
         }
-        
+        else if ($a == "sitemap.xml")
+        {
+            echo $a;
+        }
     }
+}
 ?>
