c9ca0b4063f1b24ddeea2bbe0e5c2ca0a2c5aa3f
devcuong <viettxt@gmail.com>
2020-04-17 03:24:27 +0700

    ok

---
+++ mvc/views/pages/companies-news.php
@@ -0,0 +1,296 @@
+<style type="text/css" >
+.box-border {
+    background-color: #fff;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #e5e6e9 #dfe0e4 #d0d1d5;
+    border-radius: 3px;
+}
+.detail-news {
+    padding-left: 5rem;
+}
+.detail-news {
+    position: relative;
+    font-size: 2rem;
+    padding: 1.5rem;
+	font-family: "Roboto",sans-serif;
+}
+
+.detail-news .detail-news-title {
+    font-family: 'Anton', sans-serif;
+    font-size: 2rem;
+    margin-bottom: 10px;
+}
+
+.detail-news .detail-news-posted {
+    border-bottom: 1px dotted #ccc;
+    padding-bottom: 5px;
+    margin-bottom: 20px;
+    font-size: 90%;
+    font-weight: 500;
+}
+
+.detail-news .detail-news-posted {
+    color: #4285f4;
+    margin-bottom: .5rem;
+}
+
+.mr-auto, .mx-auto {
+    margin-right: auto !important;
+}
+
+.d-flex {
+    display: -ms-flexbox !important;
+    display: -webkit-box !important;
+    display: flex !important;
+}
+
+.detail-news .detail-news-posted .author {
+    color: #b62831;
+    margin-right: 7px;
+}
+
+.detail-news .detail-news-posted .time {
+    display: block;
+    padding: 0px;
+    color: #666;
+    margin-right: 7px;
+}
+
+.detail-news .detail-news-posted .name-menu {
+    display: block;
+    padding-left: 10px;
+    color: #444;
+    text-transform: uppercase;
+    margin-right: 7px;
+    font-weight: 700;
+    position: relative;
+}
+
+.news-list-small {
+    margin-bottom: 15px;
+    margin-left: 0px !important;
+}
+
+.list-group {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    -ms-flex-direction: column;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    flex-direction: column;
+    padding-left: 0;
+    margin-bottom: 0;
+}
+
+.news-list-small .news-item {
+    margin-bottom: 7px;
+    border: none;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 0px;
+    padding-left: 0px;
+    padding-right: 0px;
+    position: relative;
+    background-image: none !important;
+}
+
+.news-list-small .news-item .news-title {
+    margin-bottom: 0px;
+    font-size: 90%;
+    color: #004383;
+    width: 100%;
+    display: block;
+    -o-text-overflow: ellipsis;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    font-weight: normal;
+}
+
+.news-list-small .news-item .news-title a {
+    font-weight: normal !important;
+    color: inherit !important;
+}
+
+.detail-content {
+    font-size: 1.2rem;
+    text-align: justify;
+}
+
+.detail-news .detail-news-desc, .detail-news h2 {
+    font-weight: 600;
+    font-style: italic;
+    margin-bottom: 1rem;
+}
+
+.detail-content .figure.fluid {
+    display: block;
+    text-align: center;
+}
+</style>
+<div class="columns" style="height: auto !important;">
+	<section class="companies column is-three-quarters"
+		style="height: auto !important;">
+		<div class="detail-news box-border">
+			<h1 class="detail-news-title">Ra mắt chưa lâu, Mitsubishi Attrage
+				tăng trưởng doanh số tới 3 lần so với tháng trước</h1>
+			<div
+				class="detail-news-posted start-social-fixed d-flex align-items-center">
+				<div class="d-flex mr-auto">
+					<span class="author"><a
+						title="Xem các bài viết của Nguyễn Trọng Hữu"
+						href="https://dailyxe.com.vn/author/nguyen-trong-huu-16a.html">Nguyễn
+							Trọng Hữu</a></span> <span class="time">đăng lúc 15-04-2020 14:30</span>
+					<span class="name-menu">Tin tức mới</span>
+				</div>
+			</div>
+			<span macd="tinlienquan_chitiettintuc"></span>
+			<ul class="list-group list-group-flush news-list-small ng-scope"
+				ng-controller="modal-content as ctrl">
+				<li class="list-group-item news-item"><h3 class="news-title">
+						<a target="_self"
+							href="https://dailyxe.com.vn/tin-tuc/hang-hot-mitsubishi-xpander-truoc-co-hoi-noi-dia-hoa-gia-con-co-the-re-hon-tai-viet-nam-5629d.html"
+							class="modal-link-content" data-id="5629"
+							ng-click="ctrl.loadTinTuc('5629', 'https://dailyxe.com.vn/tin-tuc/hang-hot-mitsubishi-xpander-truoc-co-hoi-noi-dia-hoa-gia-con-co-the-re-hon-tai-viet-nam-5629d.html')"
+							title="Hàng hot Mitsubishi Xpander trước cơ hội nội địa hóa, giá còn có thể rẻ hơn tại Việt Nam"><span
+							class="times-small"></span>Hàng hot Mitsubishi Xpander trước cơ
+							hội nội địa hóa, giá còn có thể rẻ hơn tại Việt Nam</a>
+					</h3></li>
+				<li class="list-group-item news-item"><h3 class="news-title">
+						<a target="_self"
+							href="https://dailyxe.com.vn/tin-tuc/nhung-mau-xe-phan-khuc-b-dat-khach-nhat-viet-nam-thang-72017-1444d.html"
+							class="modal-link-content" data-id="1444"
+							ng-click="ctrl.loadTinTuc('1444', 'https://dailyxe.com.vn/tin-tuc/nhung-mau-xe-phan-khuc-b-dat-khach-nhat-viet-nam-thang-72017-1444d.html')"
+							title="Những mẫu xe phân khúc B đắt khách nhất Việt Nam tháng 7/2017"><span
+							class="times-small"></span>Những mẫu xe phân khúc B đắt khách
+							nhất Việt Nam tháng 7/2017</a>
+					</h3></li>
+				<li class="list-group-item news-item"><h3 class="news-title">
+						<a target="_self"
+							href="https://dailyxe.com.vn/tin-tuc/giam-gia-bot-lai-nhieu-hang-xe-ban-chay-bat-ngo-trong-mua-dich-co-ca-nhung-cai-ten-xua-nay-e-nhat-viet-nam-12875d.html"
+							class="modal-link-content" data-id="12875"
+							ng-click="ctrl.loadTinTuc('12875', 'https://dailyxe.com.vn/tin-tuc/giam-gia-bot-lai-nhieu-hang-xe-ban-chay-bat-ngo-trong-mua-dich-co-ca-nhung-cai-ten-xua-nay-e-nhat-viet-nam-12875d.html')"
+							title="Giảm giá bớt lãi, nhiều hãng xe bán chạy bất ngờ trong mùa dịch: Có cả những cái tên xưa nay ế nhất Việt Nam"><span
+							class="times-small"></span>Giảm giá bớt lãi, nhiều hãng xe bán
+							chạy bất ngờ trong mùa dịch: Có cả những cái tên xưa nay ế nhất
+							Việt Nam</a>
+					</h3></li>
+			</ul>
+			<div class="detail-news-body detail-content clearfix" id="qcbody">
+				<h2>Trong tháng 3/2020, Mitsubishi Attrage thế hệ mới bán ra tổng
+					cộng 418 xe, gấp ba lần doanh số của tháng 2 trước đó. Đây được xem
+					là một tín hiệu rất khả quan cho mẫu sedan cỡ nhỏ tới từ Nhật Bản.</h2>
+				<p>Theo báo cáo doanh số của Hiệp hội các nhà sản xuất ô tô Việt Nam
+					(VAMA), trong tháng 3/2020 vừa qua, dù phải chịu ảnh hưởng nặng nề
+					của đại dịch Covid-19 với những diễn biến hết sức phức tạp, khiến
+					tình hình doanh số ảm đạm, nhiều nhà sản xuất phải tạm đóng cửa nhà
+					máy và đại lý. Tuy nhiên, làng xe Việt vẫn ghi nhận một số trường
+					hợp "vượt khó đi lên" thành công hết sức ấn tượng. Trong số đó,
+					không thể bỏ qua cái tên Mitsubishi Attrage thế hệ mới với doanh số
+					418 xe, gấp 3 lần so với tháng 2 trước đó.</p>
+				<p>Trước thời điểm ra mắt thế hệ hoàn toàn mới, quả thực mà nói,
+					Mitsubishi Attrage là một trong những cái tên mờ nhạt nhất trong
+					phân khúc sedan hạng B tại Việt Nam. Ở phân khúc này, đa phần người
+					dùng sẽ để ý tới những mẫu xe như Toyota Vios, Hyundai Accent, Kia
+					Soluto hay Nissan Sunny, chẳng mấy ai dành sự quan tâm cho cái tên
+					Attrage từ thương hiệu Mitsubishi, mặc cho mẫu xe này có nguồn gốc
+					nhập khẩu.</p>
+				<figure class="figure fluid">
+					<a data-fancybox="gallery"
+						href="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-1-115714j.jpg"><img
+						alt="ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc"
+						class="figure-img img-replaced lazy-loading-image loaded"
+						src="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-1-115714j.jpg"
+						style="width: 100%"
+						title="ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc">
+					<noscript>
+							<img
+								alt=ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc
+								class="figure-img fanciedbox img-replaced noscript"
+								src=https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-1-115714j.jpg
+								style="width: 100%"
+								title=ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc>
+						</noscript></a>
+				</figure>
+				<p>Tuy nhiên, mọi chuyện đã hoàn toàn thay đổi kể từ khi Mitsubishi
+					Việt Nam mang về nước mẫu Attrage thế hệ hoàn toàn mới. Bên cạnh 12
+					thay đổi đáng chú ý về thiết kế và trang bị, Mitsubishi Attrage còn
+					sở hữu giá bán cạnh tranh chỉ từ 375 - 460 triệu VNĐ, mức giá tốt
+					nhất trong phân khúc. Chính vì vậy, Mitsubishi Attrage 2020 đã
+					nhanh chóng chiếm được tình cảm của người dùng Việt và cạnh tranh
+					sát ván với các đối thủ là Kia Soluto và Nissan Sunny.</p>
+				<p>Sự thành công bước đầu của Mitsubisi Attrage 2020 được đánh giá
+					là tới từ nhiều yếu tố khác nhau. Đầu tiên chính là vấn đề giá bán.
+					Với phiên bản facelift 2020, dòng xe Attrage tại Việt Nam đã được
+					thu gọn, giảm từ 04 phiên bản trước đó thành 2 phiên bản duy nhất:
+					MT và CVT. Giá bán niêm yết của phiên bản Attrage MT 2020 là 375
+					triệu VNĐ, bản Attrage CVT 2020 cao cấp có giá bán 460 triệu VNĐ.
+					Như vậy, hai phiên bản Attrage 2020 sẽ rẻ hơn các bản tương ứng đời
+					2019 lần lượt là 25 và 15 triệu VNĐ.</p>
+				<figure class="figure fluid">
+					<a data-fancybox="gallery"
+						href="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-2-115715j.jpg"><img
+						alt="ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc"
+						class="figure-img img-replaced lazy-loading-image loaded"
+						src="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-2-115715j.jpg"
+						style="width: 100%"
+						title="ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc">
+					<noscript>
+							<img
+								alt=ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc
+								class="figure-img fanciedbox img-replaced noscript"
+								src=https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-2-115715j.jpg
+								style="width: 100%"
+								title=ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc>
+						</noscript></a>
+				</figure>
+				<p>Yêu tố thứ hai không kém phần quan trọng chính nằm ở phần thiết
+					kế. Nhờ việc áp dụng triệt để ngôn ngữ Dynamic Shield, Mitsubisi
+					Attrage 2020 giờ đây sở hữu một ngoại hình sáng, ấn tượng và đầy sự
+					trẻ trung. Ngoài ra, Attrage 2020 còn là một mẫu xe phù hợp với
+					việc di chuyển đô thị khi bán kính quay đầu chỉ 4,8 mét. Thực tế
+					cho thấy, Mitsubisi Attrage 2020 chính là mẫu xe có ngoại hình bắt
+					mắt hàng đầu trong phân khúc sedan hạng B hiện nay tại thị trường
+					Việt Nam.</p>
+				<figure class="figure fluid">
+					<a data-fancybox="gallery"
+						href="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-3-115716j.jpg"><img
+						alt="ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc"
+						class="figure-img img-replaced lazy-loading-image"
+						src="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-3-115716j.jpg?q=1"
+						style="width: 100%"
+						title="ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc"
+						data-src="https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-3-115716j.jpg">
+					<noscript>
+							<img
+								alt=ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc
+								class="figure-img fanciedbox img-replaced noscript"
+								src=https://cdn.dailyxe.com.vn/image/ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc-3-115716j.jpg
+								style="width: 100%"
+								title=ra-mat-chua-lau-mitsubishi-attrage-tang-truong-doanh-so-toi-3-lan-so-voi-thang-truoc>
+						</noscript></a>
+				</figure>
+				<p>Tiếp tới, trang bị cũng là một điểm cộng trên Mitsubisi Attrage
+					2020. Với hàng loạt tiện ích hiện đại như đèn pha LED, đèn hậu LED,
+					la-zăng 15 inch, hệ thống giải trí DVD với màn hình 7 inch hỗ trợ
+					kết nối Android Auto/Apple CarPlay, Chìa khóa thông minh, khởi động
+					nút bấm, ghế da có thiết kế mới, nút chỉnh âm thanh trên vô lăng,
+					hệ thống điều hòa tự động, gập gương điện đến những tiện nghi cho
+					hành khách sau như tựa tay với hốc để ly, 3 tựa đầu cho hàng ghế
+					sau, Cruise Control, Hệ thống chống bó cứng phanh ABS, phân phối
+					lực phanh điện tử EBD, cơ cấu căng đai tự động, túi khí đôi, móc
+					ghế an toàn cho trẻ em ISO-FIX, Camera lùi...</p>
+				<p>Về truyền động, Mitsubishi Attrage sử dụng động cơ 1.2L MIVEC kết
+					hợp với hộp số CVT INVECS-III. Khối động cơ 3 xy-lanh dung tích chỉ
+					1.2L của Attrage có công suất tối đa 78 mã lực và lực mô-men xoắn
+					cực đại 100 Nm. Đi kèm với đó là trang bị hộp số sàn và hộp số tự
+					động CVT.</p>
+				<p>Hoàng Vũ (Tuoitrethudo)</p>
+				<p class="text-right end-social-fixed">Nguồn: XeHay</p>
+			</div>
+		</div>
+	</section>
+	<section class="summary-reviews column z-1">cba</section>
+</div>
