cbf0e48ded370423b6c104cd9f2965c39d5d24ed
devcuong <viettxt@gmail.com>
2020-05-03 02:48:34 +0700

    ok

--- mvc/views/pages/companies-news.php
+++ mvc/views/pages/companies-news.php
@@ -58,157 +58,211 @@
 		</div>
 	</div>
 	<div class="column col-top-right">
-		<div class="news-right">
-			<div class="media">
-				<a class="news-right-img" target="_self"
-					title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-7">
-					<img class="lazy-loading-image loaded" style="width: 100%"
-					src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
-					alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					data-pin-nopin="true">
-
-				</a>
-				<div class="media-body has-menu-dots">
-					<h3 class="news-right-title">
-						<a target="_self"
+		<div class="card box-right box-border box-bottom">
+			<h3 class="card-header">Tin tức mới</h3>
+			<div class="card-block">
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
 							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-							href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-7">Triển
-							lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
-					</h3>
-					<h5 class="news-posted">
-						<span class="origin"><a title="hrinsider"
-							href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-7">hrinsider</a></span>
-					</h5>
+							href="http://localhost/rvcongty/companies-news/ban-can-chuan-bi-gi-cho-buoi-phong-van-online-9">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/ban-can-chuan-bi-gi-cho-buoi-phong-van-online-9">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/ban-can-chuan-bi-gi-cho-buoi-phong-van-online-9">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>10</span>
+							</p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="news-right">
-			<div class="media">
-				<a class="news-right-img" target="_self"
-					title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					href="http://localhost/rvcongty/companies-news/trien-lam-oto-frankfurt-motor-show-bi-lo-chet-yeu-neu-khong-doi-moi-6">
-					<img class="lazy-loading-image loaded" style="width: 100%"
-					src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
-					alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					data-pin-nopin="true">
-
-				</a>
-				<div class="media-body has-menu-dots">
-					<h3 class="news-right-title">
-						<a target="_self"
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
 							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-							href="http://localhost/rvcongty/companies-news/trien-lam-oto-frankfurt-motor-show-bi-lo-chet-yeu-neu-khong-doi-moi-6">Triển
-							lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
-					</h3>
-					<h5 class="news-posted">
-						<span class="origin"><a title="hrinsider"
-							href="http://localhost/rvcongty/companies-news/trien-lam-oto-frankfurt-motor-show-bi-lo-chet-yeu-neu-khong-doi-moi-6">hrinsider</a></span>
-					</h5>
+							href="http://localhost/rvcongty/companies-news/ban-can-chuan-bi-gi-cho-buoi-phong-van-online-8">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/ban-can-chuan-bi-gi-cho-buoi-phong-van-online-8">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/ban-can-chuan-bi-gi-cho-buoi-phong-van-online-8">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>20</span>
+							</p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="news-right">
-			<div class="media">
-				<a class="news-right-img" target="_self"
-					title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-5">
-					<img class="lazy-loading-image loaded" style="width: 100%"
-					src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
-					alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					data-pin-nopin="true">
-
-				</a>
-				<div class="media-body has-menu-dots">
-					<h3 class="news-right-title">
-						<a target="_self"
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
 							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-							href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-5">Triển
-							lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
-					</h3>
-					<h5 class="news-posted">
-						<span class="origin"><a title="hrinsider"
-							href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-5">hrinsider</a></span>
-					</h5>
+							href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-7">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-7">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-7">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>4</span>
+							</p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="news-right">
-			<div class="media">
-				<a class="news-right-img" target="_self"
-					title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-cung-hoang-dao-trong-thang-042020-4">
-					<img class="lazy-loading-image loaded" style="width: 100%"
-					src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
-					alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					data-pin-nopin="true">
-
-				</a>
-				<div class="media-body has-menu-dots">
-					<h3 class="news-right-title">
-						<a target="_self"
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
 							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-							href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-cung-hoang-dao-trong-thang-042020-4">Triển
-							lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
-					</h3>
-					<h5 class="news-posted">
-						<span class="origin"><a title="hrinsider"
-							href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-cung-hoang-dao-trong-thang-042020-4">hrinsider</a></span>
-					</h5>
+							href="http://localhost/rvcongty/companies-news/trien-lam-oto-frankfurt-motor-show-bi-lo-chet-yeu-neu-khong-doi-moi-6">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/trien-lam-oto-frankfurt-motor-show-bi-lo-chet-yeu-neu-khong-doi-moi-6">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/trien-lam-oto-frankfurt-motor-show-bi-lo-chet-yeu-neu-khong-doi-moi-6">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>4</span>
+							</p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="news-right">
-			<div class="media">
-				<a class="news-right-img" target="_self"
-					title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-con-giap-trong-thang-042020-3">
-					<img class="lazy-loading-image loaded" style="width: 100%"
-					src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
-					alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					data-pin-nopin="true">
-
-				</a>
-				<div class="media-body has-menu-dots">
-					<h3 class="news-right-title">
-						<a target="_self"
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
 							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-							href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-con-giap-trong-thang-042020-3">Triển
-							lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
-					</h3>
-					<h5 class="news-posted">
-						<span class="origin"><a title="hrinsider"
-							href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-con-giap-trong-thang-042020-3">hrinsider</a></span>
-					</h5>
+							href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-5">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-5">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/trac-nghiem-hinh-anh-hinh-anh-dau-tien-ban-nhin-thay-se-tiet-lo-tinh-hinh-cuoc-song-hien-tai-cua-ban-5">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>3</span>
+							</p>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-		<div class="news-right">
-			<div class="media">
-				<a class="news-right-img" target="_self"
-					title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					href="http://localhost/rvcongty/companies-news/dieu-sai-lam-nhat-trong-su-nghiep-cua-toi-do-chinh-la-qua-trung-thanh-2">
-					<img class="lazy-loading-image loaded" style="width: 100%"
-					src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
-					alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-					data-pin-nopin="true">
-
-				</a>
-				<div class="media-body has-menu-dots">
-					<h3 class="news-right-title">
-						<a target="_self"
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
 							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
-							href="http://localhost/rvcongty/companies-news/dieu-sai-lam-nhat-trong-su-nghiep-cua-toi-do-chinh-la-qua-trung-thanh-2">Triển
-							lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
-					</h3>
-					<h5 class="news-posted">
-						<span class="origin"><a title="hrinsider"
-							href="http://localhost/rvcongty/companies-news/dieu-sai-lam-nhat-trong-su-nghiep-cua-toi-do-chinh-la-qua-trung-thanh-2">hrinsider</a></span>
-					</h5>
+							href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-cung-hoang-dao-trong-thang-042020-4">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-cung-hoang-dao-trong-thang-042020-4">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-cung-hoang-dao-trong-thang-042020-4">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>1</span>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
+							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-con-giap-trong-thang-042020-3">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-con-giap-trong-thang-042020-3">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/du-doan-su-nghiep-cua-12-con-giap-trong-thang-042020-3">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>1</span>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="news-right">
+					<div class="media has-menu-dots">
+						<a class="news-right-img" target="_self"
+							title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							href="http://localhost/rvcongty/companies-news/dieu-sai-lam-nhat-trong-su-nghiep-cua-toi-do-chinh-la-qua-trung-thanh-2">
+							<img class="lazy-loading-image loaded"
+							src="http://localhost/rvcongty/mvc/public/asset/news/1603.11.1.jpg"
+							alt="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+							data-pin-nopin="true">
+						</a>
+						<div class="media-body">
+							<h3 class="news-right-title">
+								<a target="_self"
+									title="Triển lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới"
+									href="http://localhost/rvcongty/companies-news/dieu-sai-lam-nhat-trong-su-nghiep-cua-toi-do-chinh-la-qua-trung-thanh-2">Triển
+									lãm ôtô Frankfurt Motor Show bị lo chết yểu nếu không đổi mới</a>
+							</h3>
+							<p class="news-posted">
+								<span class="origin"><a title="BanXeHoi"
+									href="http://localhost/rvcongty/companies-news/dieu-sai-lam-nhat-trong-su-nghiep-cua-toi-do-chinh-la-qua-trung-thanh-2">hrinsider</a></span>
+								<span><i class="fa fa-eye"></i>1</span>
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
-<div class="columns">
-</div>
+<div class="columns"></div>
