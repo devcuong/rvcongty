3cd3939f301db13caccc3eba4643ca60e19c4b34
devcuong <cuong.dh8c@gmail.com>
2020-05-30 17:20:12 +0700

    ok

---
+++ mvc/controllers/Videos.php
@@ -0,0 +1,29 @@
+<?php
+class Videos extends Controller
+{
+    //public $VideoModel;
+    public function __construct()
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        //$this->NewsModel = $this->model("NewsModel");
+    }
+    
+    function Index(){
+        // Title
+        $title = "Review công ty ";
+        
+        // Description
+        $description = "Review về mức lương, qui trình phỏng vấn, môi trường, tuyển dụng, sếp và công việc tại ";
+        
+        // View
+        $this->view("main-template", [
+            "Page" => "news-detail",
+            "Title" => $title,
+            "Description" => $description,
+            "News" => $newsLater,
+            "NewsMoiNhat"=>$newsMoiNhat,
+            "NewsLienQuan" => $newsLienQuan
+            
+        ]);
+    }
+}
---
+++ mvc/views/pages/videos.php
@@ -0,0 +1,3 @@
+<?php
+    echo "videos";
+?>
