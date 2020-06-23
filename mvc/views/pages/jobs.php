d358ddabcae28c22bc692f373f1f915646960a06
devcuong <cuong.dh8c@gmail.com>
2020-06-23 11:46:46 +0700

    ok

--- mvc/views/pages/jobs.php
+++ mvc/views/pages/jobs.php
@@ -1,3 +1,23 @@
-<?php
-    echo "ok";
-?>
+<style>
+.job-list {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-left: -10px;
+    margin-right: -10px;
+}
+.job-item {
+    position: relative;
+    width: 20%;
+	background: blue;
+}
+</style>
+<div class="job-list">
+	<div class="job-item">a</div>
+	<div class="job-item">b</div>
+	<div class="job-item">c</div>
+	<div class="job-item">d</div>
+	<div class="job-item">d</div>
+</div>
