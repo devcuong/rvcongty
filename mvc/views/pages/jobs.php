146e233e8e0b598760f95371b974e7acc7f65fc6
devcuong <viettxt@gmail.com>
2020-06-25 09:45:36 +0700

    update

--- mvc/views/pages/jobs.php
+++ mvc/views/pages/jobs.php
@@ -1,7 +1,3 @@
-<link href="https://fonts.googleapis.com/css?family=Lato:400,600,700"
-	rel="stylesheet" />
-<link href="<?php echo $servername ?>/mvc/public/css/search.css"
-	rel="stylesheet" />
 <style>
 .job-list {
 	display: -webkit-box;
@@ -136,18 +132,6 @@
 	border-width: 20px 100px 0 0;
 	border-color: rgba(255, 255, 255, 1) transparent transparent transparent;
 }
-.s009 form .inner-form .basic-search{
-	display: flex;
-}
-.basic-search .input-search{
-	width: 40% !important;
-}
-.basic-search .select-location{
-	width: 20% !important;
-}
-.basic-search .select-category{
-	width: 20% !important;
-}
 </style>
 <script>
 $(document).ready(function(){
@@ -164,142 +148,7 @@ $(document).ready(function(){
 });
 </script>
 <div class="job-container">
-	<div class="s009">
-		<form>
-			<div class="inner-form">
-				<div class="basic-search">
-					<div class="input-field input-search">
-						<input id="search" type="text" placeholder="Type Keywords" />
-						<div class="icon-wrap">
-							<svg class="svg-inline--fa fa-search fa-w-16" fill="#ccc"
-								aria-hidden="true" data-prefix="fas" data-icon="search"
-								role="img" xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 512 512">
-                  <path
-									d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-                </svg>
-						</div>
-					</div>
-					<div class="input-field select-location">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Accessories</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-field select-category">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Accessories</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-				</div>
-				<div class="advance-search">
-					<div class="row">
-						<div class="input-field">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Accessories</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-field">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Color</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-field">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Size</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-field">
-							<div class="group-btn">
-								<button class="btn-search">SEARCH</button>
-							</div>
-						</div>
-					</div>
-					<div class="row third">
-						<div class="input-field">
-							<div class="result-count">
-								<a id="show-search">Chọn tìm kiếm nâng cao</a> <a
-									id="hide-search">Ẩn tìm kiếm nâng cao</a>
-							</div>
-
-						</div>
-					</div>
-					<div class="row second" id="advanced-search-section">
-						<div class="input-field">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Sale</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-field">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Time</option>
-									<option>Last time</option>
-									<option>Today</option>
-									<option>This week</option>
-									<option>This month</option>
-									<option>This year</option>
-								</select>
-							</div>
-						</div>
-						<div class="input-field">
-							<div class="input-select">
-								<select data-trigger="" name="choices-single-defaul">
-									<option placeholder="" value="">Type</option>
-									<option>Subject b</option>
-									<option>Subject c</option>
-								</select>
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</form>
-		<script src="<?php echo $servername ?>/mvc/public/js/choices.js"></script>
-		<script>
-      const customSelects = document.querySelectorAll("select");
-      const deleteBtn = document.getElementById('delete')
-      const choices = new Choices('select',
-      {
-        searchEnabled: false,
-        itemSelectText: '',
-        removeItemButton: true,
-      });
-      deleteBtn.addEventListener("click", function(e)
-      {
-        e.preventDefault()
-        const deleteAll = document.querySelectorAll('.choices__button')
-        for (let i = 0; i < deleteAll.length; i++)
-        {
-          deleteAll[i].click();
-        }
-      });
-    </script>
-	</div>
+	
 	<div class="job-list">
 		<div class="job-wraper">
 			<div class="job-item">
